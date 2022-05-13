af8c48414f2a8b3c8b7f918bf83092f9a84792fc
Ryota <ryota.hkho@outlook.jp>
2022-05-13 21:14:00 +0900

    add python program 2

--- show.py
+++ show.py
@@ -1,6 +1,6 @@
 import random
 
 with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakas = [s.strip() for f.readlines()]
+    wakas = [f.strip() for f.readlines()]
 
 print(wakas[random.randrange(len(wakas))])
